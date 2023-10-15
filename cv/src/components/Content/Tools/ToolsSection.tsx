542fe374f46257f9e33176114dbd2ed7f6c49c02
FreightDH <maxim.zolotkov@yandex.ru>
2023-10-15 11:42:09 +0300

    feat: add tools section component

---
+++ cv/src/components/Content/Tools/ToolsSection.tsx
@@ -0,0 +1,21 @@
+import ContentTitle from '../Title/ContentTitle';
+import ToolsItem from './Item/ToolsItem';
+import toolsInfo from './toolsInfo';
+import './ToolsSection.scss';
+
+const Tools = () => {
+  return (
+    <section className="tools">
+      <ContentTitle>Tools</ContentTitle>
+      <div className="content--content">
+        <ul className="tools__list">
+          {toolsInfo.map((tool) => (
+            <ToolsItem {...tool} />
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Tools;
